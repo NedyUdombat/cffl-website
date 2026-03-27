426f44e11f40fe7670e27e0eee5f9a22287029b8
jamesnottidge <freshice57@gmail.com>
2026-03-27 10:23:25 -0700

    update iframe

---
+++ src/app/register/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Navbar from "@/app/components/Navbar";
+import Footer from "../components/footer";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-[#13141C] text-white">
+      <Navbar linkTextColor="text-white" />
+
+      <div className="flex-grow mt-[142px]">
+        <iframe
+          src="https://playpass.com/CFFLAGOS/cffl-season-iii-RG2V2mc"
+          className="w-full h-[calc(100vh-142px)] border-0"
+          allow="payment; clipboard-write"
+          title="CFFL Season III Registration"
+        />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
